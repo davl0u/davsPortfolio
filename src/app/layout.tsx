720d8e414269904172a9b459cd1c70998b22660b
dav.lou <daviloureiro.me@gmail.com>
2024-03-18 15:29:34 -0300

    fixes commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,7 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "davsPortfolio",
-  icons: {
-    icon: "davslogo.png"
-  },
+  icons: "favico.ico",
 };
 
 export default function RootLayout({
@@ -19,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className=" scroll-smooth">
+      <head>
+        <link rel="shortcut icon" href="images/davsLogooo.png" />
+      </head>
       <body className="flex flex-col min-h-screen w-full font-montserrat text-white">
         <Header />
         {children}
